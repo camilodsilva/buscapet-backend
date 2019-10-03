ff2374e4ae37a8181e1f547a9a2e6844cfd6b5f1
CamiloSilvaD <camilodsmp@gmail.com>
2019-10-03 19:29:11 -0300

    Update session controller and user model. Create user migration

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -1,13 +1,23 @@
 import jwt from 'jsonwebtoken';
 
 import User from '../models/User';
+import File from '../models/File';
 import authConfig from '../../config/auth';
 
 class SessionController {
   async store(req, res) {
     const { email, password } = req.body;
 
-    const user = await User.findOne({ where: { email } });
+    const user = await User.findOne({
+      where: { email },
+      include: [
+        {
+          model: File,
+          as: 'avatar',
+          attributes: ['id', 'path', 'url'],
+        },
+      ],
+    });
 
     if (!user) {
       return res.status(401).json({ error: 'Invalid email' });
@@ -17,13 +27,15 @@ class SessionController {
       return res.status(401).json({ error: 'Invalid password' });
     }
 
-    const { id, name } = user;
+    const { id, name, is_shelter_owner, avatar } = user;
 
     return res.json({
       user: {
         id,
         name,
         email,
+        is_shelter_owner,
+        avatar,
       },
       token: jwt.sign({ id }, authConfig.secret, {
         expiresIn: authConfig.expiresIn,
